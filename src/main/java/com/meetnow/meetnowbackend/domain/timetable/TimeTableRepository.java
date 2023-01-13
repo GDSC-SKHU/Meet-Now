6977610dd008e522376cf758aa33241ceee0362c
vact19 <chj6703@gmail.com>
2023-01-13 18:15:26 +0900

    uopdate

--- src/main/java/com/meetnow/meetnowbackend/domain/timetable/TimeTableRepository.java
+++ src/main/java/com/meetnow/meetnowbackend/domain/timetable/TimeTableRepository.java
@@ -4,12 +4,10 @@ import com.meetnow.meetnowbackend.domain.room.Room;
 import com.meetnow.meetnowbackend.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface TimeTableRepository extends JpaRepository<TimeTable, Long> {
     TimeTable findByUser(User user);
 
 
 
-    Optional<TimeTable> findByUserAndRoom(User user, Room room);
+    TimeTable findByUserAndRoom(User user, Room room);
 }
